c084904568bb2adeec28a4e183db0066e96b953b
ThePlatypusEveryoneLoves <atharv.sondhi@gmail.com>
2023-08-27 16:00:21 -0500

    Use templates for layers

--- Network.cpp
+++ Network.cpp
@@ -1,17 +1,25 @@
 #include "Layer.cpp"
 #include <vector>
+#include <array>
+#include <cassert>
+#include <concepts>
 
+template<size_t start, size_t middle, size_t... ending>  //!second ending if more then two layers
 struct NeuralNetwork {
 	using Matrix2D = std::vector<std::vector<double>>;
 	using Matrix1D = std::vector<double>;
 	friend Layer;
-	std::vector<Layer> Layers;
-	size_t InputSize;
-	NeuralNetwork(std::vector<size_t> LayerSizes) {
-		if (LayerSizes.size() < 2) {
-			throw std::length_error("Must be at least 2 Layers");
-		}
-		InputSize = LayerSizes[0];
+	/*
+	!the following line may depend on "smart" compilers
+	!that can make the "+2" a constexpr
+	TODO: Make a non "smart compiler" version of this
+	 */
+	std::array<Layer,sizeof...(ending)+2> Layers { }; 
+	
+	size_t InputSize = start;
+	size_t OutputSize { };
+	NeuralNetwork() {
+		std::array<size_t,sizeof...(ending)+2> LayerSizes{start, middle, (ending)... };
 
 		for (size_t i = 0; i < LayerSizes.size() - 1; i++) {
 			Layers.emplace_back(LayerSizes[i], LayerSizes[i + 1]);
@@ -40,45 +48,42 @@ struct NeuralNetwork {
 
 
 int main() {
-	NeuralNetwork network({ 3,1 });
-	std::vector<double> l{ 1,2,3 };
-	network.CalculateOutputs(l);
-	for (auto x : l) {
-		std::cout << x << std::endl;
-	}
+	// NeuralNetwork network({ 3,1 });
+	// std::vector<double> l{ 1,2,3 };
+	// network.CalculateOutputs(l);
+	// assert(l == std::vector<double>{0});
 
+    //  /*
+	//     Layer MyLayer(
+	//                         {
+	//                             {1,1,1,1},
+	//                             {2,2,2,2},
+	//                             {3,3,3,3}
+	//                         },
+	//                         {-1,1,-1,1}
+	//                     );
+	//                    
+	//    
+	//     MyLayer.CalculateOutputs(l);
+	//    for(auto x:l){
+	//         std::cout<<x<<std::endl;
+	//     }*/
 
-     /*
-	    Layer MyLayer(
-	                        {
-	                            {1,1,1,1},
-	                            {2,2,2,2},
-	                            {3,3,3,3}
-	                        },
-	                        {-1,1,-1,1}
-	                    );
-	                   
-	   
-	    MyLayer.CalculateOutputs(l);
-	   for(auto x:l){
-	        std::cout<<x<<std::endl;
-	    }*/
-
-	std::vector<Layer> ll(10, Layer({ {} }, {}));
-	ll[0].SetAll({ {1,1,1,1},{2,2,2,2},{3,3,3,3} }, { 0,0,0,0 });
-     /*
-	    MyLayer.SetAll(
-	                        {
-	                            {1,1,1,1},
-	                            {2,2,2,2},
-	                            {3,3,3,3}
-	                        },
-	                        {1,-1,1,-1}
-	                    );
-	                    */
-	l = { 1,2,3 };
-	ll[0].CalculateOutputs(l);
-	for (auto x : l) {
-		std::cout << x << std::endl;
-	}
+	// std::vector<Layer> ll(10, Layer({ {} }, {}));
+	// ll[0].SetAll({ {1,1,1,1},{2,2,2,2},{3,3,3,3} }, { 0,0,0,0 });
+    //  /*
+	//     MyLayer.SetAll(
+	//                         {
+	//                             {1,1,1,1},
+	//                             {2,2,2,2},
+	//                             {3,3,3,3}
+	//                         },
+	//                         {1,-1,1,-1}
+	//                     );
+	//                     */
+	// l = { 1,2,3 };
+	// ll[0].CalculateOutputs(l);
+	// for (auto x : l) {
+	// 	std::cout << x << std::endl;
+	// }
 }
